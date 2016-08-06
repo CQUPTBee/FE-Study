bd59c10c2c762a5464fb602d082e7de52f9c6ed8
zhangboyuan-XD <zhangboyuan_xd@outlook.com>
2016-08-06 19:59:21 +0800

    final update
    all bug fixed.

--- work/zhangboyuan/stage3/task1/5-caculator/calculate.js
+++ work/zhangboyuan/stage3/task1/5-caculator/calculate.js
@@ -2,7 +2,7 @@
  *     Created by Boyuan on 2016.7.29
  */
 
-window.onload = function () {
+window.onload = function() {
     activeCalculator();
 };
 
@@ -14,8 +14,8 @@ function activeCalculator() {
     //获取按钮
     var ca_index = 0;
 
-    var add = document.getElementById("add");   //1
-    var sub = document.getElementById("sub");   //2
+    var add = document.getElementById("add");
+    var sub = document.getElementById("sub");
     var mult = document.getElementById("multiple");
     var div = document.getElementById("division");
     var to_percent = document.getElementById("to-percent");
@@ -24,89 +24,84 @@ function activeCalculator() {
     var dot = document.getElementById("dot");
     var equal = document.getElementById("equal");
     var nums = new Array(10);
-    (function () {
+    (function() {
         for (var i = 0; i < 10; i++) {
             nums[i] = document.getElementById("num-" + String(i));
         }
     })();
 
     //绑定按钮事件
-    add.onclick = function fadd() {
+    add.onclick = function() {
         result = val;
-        console.log("val的类型是: " + typeof val);
-        console.log("result的类型是: " + typeof result);
-
         val = "";
         ca_index = 1;
     };
-    sub.onclick = function fsub() {
+    sub.onclick = function() {
         result = val;
         val = "";
         ca_index = 2;
     };
-    mult.onclick = function fmult() {
+    mult.onclick = function() {
         result = val;
         val = "";
         ca_index = 3;
     };
 
-    div.onclick = function () {
+    div.onclick = function() {
         result = val;
         val = "";
         ca_index = 4;
     };
-    to_percent.onclick = function () {
-        val = val._toPercent();
-        output(val);
+    to_percent.onclick = function() {
         val = parseFloat(val);
-        result = val;
+        val = val / 100;
+        output(val * 100 + "%");
     };
-    not.onclick = function () {
+    not.onclick = function() {
         val = -val;
         result = val;
         output(result);
     };
-    AC.onclick = function () {
+    AC.onclick = function() {
         result = 0;
         val = "";
         output(result);
     };
-    dot.onclick = function () {
+    dot.onclick = function() {
         if (val.toString().split(".").length > 1) {
             //判断是否已经为浮点数
             output(val);
-        }
-        else {
+        } else {
             val = val + ".";
             output(val);
         }
     };
-    equal.onclick = function () {
+    equal.onclick = function() {
         switch (ca_index) {
             case 1:
-                val = Number(val);
-                result = Number(result);
+                val = parseFloat(val);
+                result = parseFloat(result);
                 result = result + val;
                 val = result;
                 output(result);
                 break;
             case 2:
-                val = Number(val);
-                result = Number(result);
+                val = parseFloat(val);
+                result = parseFloat(result);
                 result = result - val;
                 val = result;
                 output(result);
                 break;
             case 3:
-                val = Number(val);
-                result = Number(result);
+                val = parseFloat(val);
+                result = parseFloat(result);
                 result = result._mult(val);
                 val = result;
                 output(result);
                 break;
             case 4:
-                val = Number(val);
-                result = Number(result);
+                val = parseFloat(val);
+                result = parseFloat(result);
                 result = result._div(val);
                 val = result;
                 output(result);
@@ -115,10 +110,10 @@ function activeCalculator() {
     };
 
     //输入
-    (function () {
+    (function() {
         for (var i = 0; i < nums.length; i++) {
-            nums[i].onclick = function (_i) {
-                return function () {
+            nums[i].onclick = function(_i) {
+                return function() {
                     val = val + _i.toString();
                     output(val);
                 }
@@ -127,72 +122,51 @@ function activeCalculator() {
     })();
 
     //除法
-    Number.prototype._div = function (arg) {
+    Number.prototype._div = function(arg) {
         var r = this / arg;
 
-        if (typeof (r.toString().split(".")[1]) == "undefined")
+        if (typeof(r.toString().split(".")[1]) == "undefined")
             return r;
         if (r.toString().split(".")[1].length > 12) {
             r = r.toFixed(12);
-            r=parseFloat(r);
+            r = parseFloat(r);
             return r;
         }
         return r;
     };
 
     //乘法
-    Number.prototype._mult = function (arg) {
+    Number.prototype._mult = function(arg) {
         var r = this * arg;
 
-        if (typeof (r.toString().split(".")[1]) == "undefined")
+        if (typeof(r.toString().split(".")[1]) == "undefined")
             return r;
         else if (r.toString().split(".")[1].length > 12) {
             r = r.toFixed(12);
             r = parseFloat(r);
             return r;
-        }
-        else {
+        } else {
             return r;
         }
     };
 
 
     //百分数转换
-    Number.prototype._toPercent = function () {
+    Number.prototype._toPercent = function() {
         var r = this / 100;
 
-        if (typeof (r.toString().split(".")[1]) == "undefined")
+        if (typeof(r.toString().split(".")[1]) == "undefined")
             return r + "%";
-        if (r.toString().split(".")[1].length > 12) {
+        else if (r.toString().split(".")[1].length > 12) {
             r = r.toFixed(12);
+            r = parseFloat(r);
+            return r;
+        } else {
             return r + "%";
         }
-        return r + "%";
     };
 
     function output(something) {
         output_box.innerHTML = String(something);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
