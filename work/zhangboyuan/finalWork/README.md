81577167739de2125d805a4f38ca5c1b6854eb58
zhangboyuan-XD <zhangboyuan_xd@outlook.com>
2016-09-01 23:07:41 +0800

    完成

---
+++ work/zhangboyuan/finalWork/README.md
@@ -0,0 +1,12 @@
+## NetEaseEDU
+网易教育产品部首页。
+
+技术选型：
+
+- 模块化: requireJS
+- 模板引擎: juicer
+- CSS预编译: SASS
+- 自动化构建: gulp
+- 版本控制: git/github
+- js库/插件: jQuery、jqueryCookies
+
---
+++ work/zhangboyuan/finalWork/src/static/js/api/api.config.js
@@ -0,0 +1,12 @@
+/**
+ *  Created by boyuan on 8/29/16.
+ */
+
+define(function () {
+    return {
+        getCoures: 'http://study.163.com/webDev/couresByCategory.htm',
+        getHotcoures: 'http://study.163.com/webDev/hotcouresByCategory.htm',
+        changeFollowState: 'http://study.163.com/webDev/attention.htm',
+        login: 'http://study.163.com/webDev/login.htm'
+    }
+});
