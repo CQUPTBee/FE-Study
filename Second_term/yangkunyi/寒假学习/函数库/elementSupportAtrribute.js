7df49abe6cda65a0d01e206c8182aca2331d488c
HuangCongQing <1756260160@qq.com>
2017-03-29 14:58:03 +0800

    Revert "yangkunyi"

--- Second_term/yangkunyi/寒假学习/函数库/elementSupportAtrribute.js
+++
@@ -1,6 +0,0 @@
-//检查特定的属性
-function elementSupportAtrribute(elementName,attribute){
-	if (!document.createElement) {return false;}
-	var temp=document.createElement(elementName);
-	return(attribute in test);
-}
--- Second_term/yangkunyi/寒假学习/函数库/getNextElement.js
+++
@@ -1,9 +0,0 @@
-function getNextElement(node) {
-	if (node.nodeType == 1) {
-		return node;
-	}
-	if (node.nextSibling) {
-		return getNextElement(node.nextSibling);
-	}
-	return null;
-}
